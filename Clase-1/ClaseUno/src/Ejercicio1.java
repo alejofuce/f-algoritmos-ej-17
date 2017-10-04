787c88ba06e4f8dd634e576f2463709ea1d5b250
Alejandro Fuertes <alejofuce@gmail.com>
2017-10-04 11:50:01 -0500

    Clase-1

---
+++ Clase-1/ClaseUno/src/Ejercicio1.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author eafuc
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Programa para imprimir tabla de multiplicar de numero dado por usuario
+        Scanner e = new Scanner(System.in);
+        
+        int num;
+        
+        System.out.println("Ingrese el numero del que desea la tabla");
+        
+    }
+    
+}
